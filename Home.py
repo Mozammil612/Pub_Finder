438b3f660383487d277874d0e23de95aac3ba2ac
Syed Mozammil Moiz <118210845+Mozammil612@users.noreply.github.com>
2023-05-18 11:05:09 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title('Welcome to Closest Open Pub Finder Page')
+
+df = pd.read_csv(r'C:\Users\mozammil\final_data.csv')
+
+
+st.subheader("Basic Information about the Data.")
+st.write(f"Number of Rows: {df.shape[0]}")
+st.write(f'Number of Columns: {df.shape[1]}')
+
+st.write("Names of Columns in the dataset:")
+st.write(df.columns.T)
+
+st.subheader("Preview of the Data")
+st.write("head()")
+st.write(df.head())
+st.write("tail()")
+st.write(df.tail())
+
+
+# Show some statistics
+st.subheader(f"basic statistics about the pubs in uk:")
+st.write(df.describe())
---
+++ Pub_finder.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import folium
+from streamlit_folium import folium_static
+import pandas as pd
+
+
+df = pd.read_csv(r'C:\Users\mozammil\final_data.csv')
+
+st.sidebar.title("Find the nearest Pub")
+
+st.title("Know the Nearest Pubs in Your Area")
+
+
+# Ask the user for their location
+lat = st.text_input("Enter your latitude:")
+lon = st.text_input("Enter your longitude:")
+
+# Convert the user's location to floats, checking for empty input
+if lat and lon:
+    lat = float(lat)
+    lon = float(lon)
+
+    # Compute the Euclidean distance between the user's location and each pub's location
+    df['dist'] = ((df['latitute'] - lat) ** 2 + (df['longitute'] - lon) ** 2) ** 0.5
+
+    # Select the nearest 5 pubs
+    nearest_pubs = df.sort_values(by='dist').head(5)
+    for _, pub in nearest_pubs.iterrows():
+        
+        st.write(f"Distance: {pub['dist']:.2f} km")
+        st.write("")
+
+
+
+
+    # Create a map and add markers for each of the nearest pubs
+    m = folium.Map(location=[lat, lon], zoom_start=12)
+
+    for _, pub in nearest_pubs.iterrows():
+        folium.Marker(location=[pub['latitute'], pub['longitute']], popup=pub['pub']).add_to(m)
+
+    folium_static(m)
+else:
+    st.write("Please enter your latitude and longitude.")
